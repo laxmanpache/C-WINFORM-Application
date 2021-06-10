3f13e4c74d4046ba91ba639a24e8c1d5ebc66b18
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 20:31:45 +0530

    Add files via upload
    LOGIN SYSTEM

---
+++ LoginSystem/LoginSystem/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("dfdsfds");
+            SqlConnection con = new SqlConnection(@"integrated security=true; data source=DESKTOP-RB3HGFJ\SQLEXPRESS;initial catalog=Traning");
+            SqlDataAdapter ad = new SqlDataAdapter("Select Count(*) from EMPLOY where ENO='"+ textBox1.Text+"'and ENAME= '"+ textBox2.Text+"'",con);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+
+                this.Hide();
+                Form2 ss = new Form2();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("Enter Correct Uname And Pass");
+            }
+
+            
+        }
+    }
+}
---
+++ LoginSystem/LoginSystem/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSystem
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form2_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
